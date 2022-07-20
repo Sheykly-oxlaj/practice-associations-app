630ce4b8376f706c349084b0558c36de8e2f1a5d
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-07-20 08:48:09 -0700

    adds associations and validations

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,2 +1,7 @@
 class Meeting < ApplicationRecord
+  has_many :speakers
+  validates :title, presence: true
+  validates :agenda, presence: true
+  validates :location, presence: true
+  validates :time, presence: true
 end
--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,2 +1,6 @@
 class Speaker < ApplicationRecord
+  has_many :meetings
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
 end
